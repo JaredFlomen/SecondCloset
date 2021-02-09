41567d14c5578cba81b178090fea7c8d314822c4
Jared Flomen <jaredflomen@gmail.com>
2021-02-09 11:27:58 -0500

    Refactoring

--- server/helpers.js
+++
@@ -1,8 +0,0 @@
-const customerAPricing = (fee, items) => {
-  const basePrice = items.length * fee;
-  const discount = basePrice * 0.1;
-  const finalPrice = basePrice - discount;
-  return finalPrice;
-};
-
-modeule.exports = { customerAPricing };
--- server/index.js
+++ server/index.js
@@ -5,14 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-import customerAPricing from './helpers';
-
 app.post('/api', (req, res) => {
   const { fee, customer, items } = req.body;
   if (customer === 'A') {
-    customerAPricing(fee, items).then(response =>
-      res.send(JSON.stringify(response))
-    );
+    const basePrice = items.length * fee;
+    const discount = basePrice * 0.1;
+    const finalPrice = basePrice - discount;
+    res.send(JSON.stringify(finalPrice));
   } else if (customer === 'B') {
     const basePrice = items.length * fee;
     let volumeAccumulator = 0;
